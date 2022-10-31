893ea169a61d838b7b8f290765560d6144300f05
Tiago Lino <tiagobrasil18@gmail.com>
2022-10-31 06:52:03 -0300

    Study section finalization

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinando transição/animação de elementos/components no Vue.</p>
+      </div>
+    </div>
+    <div class="container">
+      <h3 class="font-weight-light">Animações de estado</h3>
+
+      <div class="form-group">
+        <input class="form-control" v-model="numero" step="50" />
+      </div>
+
+      <div class="alert alert-info">
+        <h3 class="font-weight-light">
+          <strong>Número: </strong>
+          <span>{{ numeroAnimado }}</span>
+        </h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { gsap } from 'gsap'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      numero: 0,
+      numeroInterpolado: 0
+    };
+  },
+  computed: {
+    numeroAnimado() {
+      return this.numeroInterpolado.toFixed(0)
+    }
+  },
+  watch: {
+    // eslint-disable-next-line no-unused-vars
+    numero(novoNumero, antigoNumero) {
+      gsap.to(this.$data, { duration: 2, numeroInterpolado: novoNumero })
+    }
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
